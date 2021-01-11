4a4ff307cf1071d4a01e324e9cd3bf74e27c4900
Lu Hernandez <guicholh@gmail.com>
2021-01-11 11:15:18 -0600

    added seed file to add plants to db

---
+++ plant_seed.sql
@@ -0,0 +1,34 @@
+-- 1
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) 
+VALUES ('Tutorial Flower', 0, 0, 00.01, 40, 10, 'Isle de Mikes', 'A Simple flower to teach you the ways of the garden.');
+
+-- 2
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) 
+VALUES ('Emmay Bud', 2, 1, 05.00, 5, 3, 'Kahului', 'Strange bud that comes from a far away island.');
+
+-- 3
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('J Hideout', 2, 1, 05.00, 5, 3, 'Missouri', 'Inexplicably fades in and out of view as if seen through a low-quality camera.');
+
+-- 4
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Silly-Dilly', 3, 2, 08.00, 5, 5, 'Ellenton', 'Is said to be the top in the leather community but no one knows what he means because he never leaves his garden.');
+
+-- 5
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Jerelily', 3, 2, 08.00, 5, 5, 'Quebec', 'Though not native to the reagion, the Jerelily found its way to the frozen area of the North.');
+
+-- 6
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Timint', 5, 3, 11.00, 5, 9, 'New Orleans', 'The Timint has forced itself to become purple through sheer wilpower.');
+
+-- 7
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Augie Beauty', 7, 3, 11.00, 5, 11, 'Chicago', 'If you ever need a link for any topic the Augie Beauty has you covered.');
+
+-- 8
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Geovannirod', 10, 4, 14.00, 5, 14, 'River Grove', 'The Geovannirod has a heart of gold and legend says it has written a book about soccer.');
+
+-- 9
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Heather', 10, 4, 14.00, 5, 14, 'Columbia', 'You will often see the Heather flower solving puzzles while dreaming about going for a nice hike.');
+
+-- 10
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Tom Blossom', 12, 5, 17.00, 5, 17, 'Zoomtopia', 'Often near a brick surrounding. The Tom Blossom gets excited when talking about bank drop boxes.');
+
+-- 11
+INSERT INTO gardens_plant(flower_name, cost, level, time_to_mature, exp_value, currency, region, description) VALUES ('Noalion', 12, 5, 17.00, 5, 17, 'Slackville', 'Ever so entergetic, the Noalion spends time being surrounded by birds.');
